4efd21d6a88fd7f72fefde34bc935cf6e8d653f9
Lucas Prates <100208473+DevLucasP@users.noreply.github.com>
2024-04-28 19:06:57 -0300

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,17 @@
+const about = document.querySelector('.inicio');
+
+about.addEventListener('click', () =>{
+    window.scroll({top: 580, behavior: "smooth"})
+})
+
+const portifolio = document.querySelector('.graus');
+
+portifolio.addEventListener('click', () =>{
+    window.scroll({top: 1150, behavior: "smooth"})
+})
+
+const contato = document.querySelector('.projeto');
+
+contato.addEventListener('click', () =>{
+    window.scroll({top: 1750, behavior: "smooth"})
+})
