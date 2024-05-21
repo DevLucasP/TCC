ae6142b62f4b604e1b15ec0ae825d9c5f725c8b4
Lucas Prates <100208473+DevLucasP@users.noreply.github.com>
2024-05-20 23:30:44 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,60 +5,20 @@ about.addEventListener('click', () =>{
     window.scroll({top: 610, behavior: "smooth"})
 })
 
-const portifolio = document.querySelector('.graus');
+const portifolio = document.querySelector('#causas');
 
 portifolio.addEventListener('click', () =>{
     window.scroll({top: 610, behavior: "smooth"})
 })
 
-const contato = document.querySelector('.projeto');
+const contato = document.querySelector('#graus');
 
 contato.addEventListener('click', () =>{
     window.scroll({top: 610, behavior: "smooth"})
 })
 
 /* Menu */
-class MobileNavbar {
-  constructor(menu, navList, navLinks) {
-    this.menu = document.querySelector(menu);
-    this.navList = document.querySelector(navList);
-    this.navLinks = document.querySelectorAll(navLinks);
-    this.activeClass = "active";
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  animateLinks() {
-    this.navLinks.forEach((link, index) => {
-      link.style.animation
-        ? (link.style.animation = "")
-        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
-            index / 7 + 0.3
-          }s`);
-    });
-  }
-
-  handleClick() {
-    this.navList.classList.toggle(this.activeClass);
-    this.menu.classList.toggle(this.activeClass);
-    this.animateLinks();
-  }
-
-  addClickEvent() {
-    this.menu.addEventListener("click", this.handleClick);
-  }
-
-  init() {
-    if (this.menu) {
-      this.addClickEvent();
-    }
-    return this;
-  }
-}
-
-const mobileNavbar = new MobileNavbar(
-  ".menu",
-  ".nav-list",
-  ".nav-list li",
-);
-mobileNavbar.init();
+window.addEventListener("scroll", function(){
+  let header = document.querySelector('#header')
+  header.classList.toggle('rolagem', window.scrollY > 400)
+})
