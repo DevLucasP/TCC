273f91870d45fa76ffc4f3f7cf7047872848ca43
Lucas Prates <100208473+DevLucasP@users.noreply.github.com>
2024-06-03 17:21:20 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,13 @@ contato.addEventListener('click', () =>{
 })
 
 /* Menu */
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
+    }
+}
